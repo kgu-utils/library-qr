7106f2d72c6f924af65b956aa4867f3f370fe155
박금혁 <ghpark@enki.co.kr>
2025-04-08 23:53:48 +0900

    Fix manifest for github pages

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,13 +13,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#3b82f6",
     icons: [
       {
-        src: "/web-app-manifest-192x192.png",
+        src: "/library-qr/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/web-app-manifest-512x512.png",
+        src: "/library-qr/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
